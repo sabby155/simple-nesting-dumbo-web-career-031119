b520e238af07b10fa6de9fd93fdf08822cd2af06
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-14 21:28:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
      }
 
     programmer_hash[0] = :yukihiro_matsumoto 
-    programmer_hash[:yukihiro_matsumoto] => {}
+    programmer_hash[:yukihiro_matsumoto] = {}
     :yukihiro_matsumoto[:known_for] = "Ruby"
     :yukihiro_matsumoto[:languages] = ["LISP", "C"]
     
